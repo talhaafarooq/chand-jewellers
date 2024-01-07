f5a932c84dab079e0c7629443d868a0f5fe57d23
Talha Farooq <talhafarooq522446@gmail.com>
2024-01-07 10:42:07 -0800

    show different datas on dashboard page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\OrderStatusEnum;
+use App\Models\Category;
+use App\Models\ContactUs;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\SubCategory;
+use App\Models\Subscribers;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function adminDashboard()
     {
-        return view('admin.dashboard');
+        $totalCategories = Category::count();
+        $totalSubCategories = SubCategory::count();
+        $totalProducts = Product::count();
+        $totalOrders = Order::count();
+        $totalNewOrders = Order::where('status',OrderStatusEnum::Received)->count();
+        $totalSubscribers = Subscribers::count();
+        $totalContatUs = ContactUs::count();
+        $totalVisitors = Visitor::count();
+        return view('admin.dashboard',
+        compact(
+            'totalCategories',
+            'totalSubCategories',
+            'totalProducts',
+            'totalOrders',
+            'totalNewOrders',
+            'totalSubscribers',
+            'totalContatUs',
+            'totalVisitors'
+        ));
     }
 }
--- resources/views/components/admin-footer.blade.php
+++ resources/views/components/admin-footer.blade.php
@@ -1,6 +1,6 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; <a href="{{ SettingsHelper::info()->website }}" target="_blank" class="text-decoration-none">{{ SettingsHelper::info()->name }}</a></strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
